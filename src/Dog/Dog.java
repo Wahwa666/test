940ac603336bf6404e93e7e261e5f98a438475d7
4kul4 <maletskyamargarita@gmail.com>
2020-09-02 18:20:36 +0300

    Initial commit

---
+++ src/Dog/Dog.java
@@ -0,0 +1,11 @@
+package Dog;
+import java.lang.*;
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog (String n, int a) {
+        name = n;
+        age = a;
+    }
+}
